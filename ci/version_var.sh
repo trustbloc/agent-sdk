759865f4aa9244fc78ce8ae21d8f7a6557b36c5c
Andrii Soluk <isoluchok@gmail.com>
2020-10-21 19:18:59 +0300

    ci: JS publish npm package
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

---
+++ ci/version_var.sh
@@ -0,0 +1,23 @@
+# Release Parameters
+BASE_VERSION=0.1.5
+IS_RELEASE=false
+
+SOURCE_REPO=agent-sdk
+RELEASE_REPO=trustbloc/${SOURCE_REPO}
+SNAPSHOT_REPO=trustbloc-cicd/snapshot
+NPM_PKG_NAME=${RELEASE_REPO}
+
+if [ ${IS_RELEASE} = false ]
+then
+  EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
+  PROJECT_VERSION=${BASE_VERSION}-${EXTRA_VERSION}
+  PROJECT_PKG_REPO=${SNAPSHOT_REPO}
+  NPM_PKG_NAME=trustbloc-cicd/${SOURCE_REPO}
+else
+  PROJECT_VERSION=${BASE_VERSION}
+  PROJECT_PKG_REPO=${RELEASE_REPO}
+fi
+
+export NPM_PKG_TAG=${PROJECT_VERSION}
+export NPM_PKG_NAME=${NPM_PKG_NAME}
+export NPM_PKG_REPO=${PROJECT_PKG_REPO}
