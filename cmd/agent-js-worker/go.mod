ecdf2dd60884227271746ca4ee683abbcf7a7b3c
Firas Qutishat <firas.qutishat@securekey.com>
2020-11-27 14:22:20 -0500

    feat: add edv caching
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- cmd/agent-js-worker/go.mod
+++ cmd/agent-js-worker/go.mod
@@ -13,8 +13,8 @@ require (
 	github.com/golang/snappy v0.0.2 // indirect
 	github.com/google/tink/go v1.5.0
 	github.com/google/uuid v1.1.2
-	github.com/hyperledger/aries-framework-go v0.1.5-0.20201124194436-a37f1c10fd4e
-	github.com/hyperledger/aries-framework-go/component/storage/jsindexeddb v0.0.0-20201124194436-a37f1c10fd4e
+	github.com/hyperledger/aries-framework-go v0.1.5-0.20201127190801-509fc3277e1e
+	github.com/hyperledger/aries-framework-go/component/storage/jsindexeddb v0.0.0-20201127190801-509fc3277e1e
 	github.com/jinzhu/copier v0.0.0-20201025035756-632e723a6687 // indirect
 	github.com/kilic/bls12-381 v0.0.0-20201022102231-585cb842ccd9 // indirect
 	github.com/mitchellh/mapstructure v1.3.3
---
+++ pkg/storage/jsindexeddbcache/db_test.go
@@ -0,0 +1,79 @@
+// +build js,wasm
+
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package jsindexeddbcache
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const sampleDBName = "testdb"
+
+func TestProvider(t *testing.T) {
+	t.Run("Test provider with db name", func(t *testing.T) {
+		prov, err := NewProvider(sampleDBName, 5*time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, prov)
+	})
+}
+
+func TestClear(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		prov, err := NewProvider(sampleDBName, 2*time.Second)
+		require.NoError(t, err)
+		store, err := prov.OpenStore("test")
+		require.NoError(t, err)
+
+		const key = "did:example:123"
+		data := []byte("value")
+
+		err = store.Put(key, data)
+		require.NoError(t, err)
+
+		doc, err := store.Get(key)
+		require.NoError(t, err)
+		require.NotEmpty(t, doc)
+		require.Equal(t, data, doc)
+
+		time.Sleep(3 * time.Second)
+
+		_, err = store.Get(key)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "data not found")
+	})
+
+	t.Run("test clear on open store", func(t *testing.T) {
+		prov, err := NewProvider(sampleDBName, 60*time.Second)
+		require.NoError(t, err)
+		store, err := prov.OpenStore("test")
+		require.NoError(t, err)
+
+		const key = "did:example:123"
+		data := []byte("value")
+
+		err = store.Put(key, data)
+		require.NoError(t, err)
+
+		doc, err := store.Get(key)
+		require.NoError(t, err)
+		require.NotEmpty(t, doc)
+		require.Equal(t, data, doc)
+
+		prov, err = NewProvider(sampleDBName, 60*time.Second)
+		require.NoError(t, err)
+		_, err = prov.OpenStore("test")
+		require.NoError(t, err)
+
+		_, err = store.Get(key)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "data not found")
+	})
+}
