98b7f7f2a0ddd5dc0727f245a84cae1b7dab07f9
Anton Biriukov <anton.biriukov@avast.com>
2022-10-28 20:01:05 +0200

    fix: fixed wallet-js-sdk tests
    fix: disabled json-ld context validation
    
    fix: update udc-vc id to unique value
    
    Signed-off-by: Anton Biriukov <anton.biriukov@avast.com>

--- cmd/wallet-js-sdk/test/specs/universal/vcwallet.spec.js
+++ cmd/wallet-js-sdk/test/specs/universal/vcwallet.spec.js
@@ -11,11 +11,9 @@ import { v4 as uuidv4 } from "uuid";
 import { getJSONTestData, loadFrameworks, testConfig } from "../common";
 import { VerifierAdapter } from "../mocks/adapters";
 import {
-  connectToMediator,
   contentTypes,
   createWalletProfile,
   DIDManager,
-  getMediatorConnections,
   UniversalWallet,
 } from "../../../src";
 
@@ -23,16 +21,13 @@ chai.use(chaiAsPromised);
 
 const WALLET_USER = "wallet-lite-user";
 const RELYING_PARTY = "relying-party-01";
-const keyType = "ED25519";
-const signatureType = "Ed25519VerificationKey2018";
 
 let walletAgent, rp, sampleMetadata;
-
 
 before(async function () {
   walletAgent = await loadFrameworks({
     name: WALLET_USER,
-    contextProviderURL: ["http://localhost:10096/agent-startup-contexts.json"]
+    contextProviderURL: ["http://localhost:10096/agent-startup-contexts.json"],
   });
 
   rp = new VerifierAdapter(RELYING_PARTY);
@@ -53,9 +48,9 @@ describe("Lite wallet tests", async function () {
     });
   });
 
-  let auth;
+  let auth, wallet;
   it("wallet user opens wallet", async function () {
-    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
+    wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
     let authResponse = await wallet.open({
       localKMSPassphrase: testConfig.walletUserPassphrase,
     });
@@ -64,8 +59,6 @@ describe("Lite wallet tests", async function () {
   });
 
   it("wallet user adds contents to wallet", async function () {
-    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
-
     // save sample metadata.
     await wallet.add({
       auth,
@@ -85,8 +78,6 @@ describe("Lite wallet tests", async function () {
   });
 
   it("wallet user adds, removes, gets, gets all contents from wallet", async function () {
-    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
-
     let ids = [uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4()];
 
     // add few sample data
@@ -133,89 +124,54 @@ describe("Lite wallet tests", async function () {
   });
 
   it("wallet user creates a key pair inside wallet", async function () {
-    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
-
     let keyPair = await wallet.createKeyPair(auth, { keyType: "ED25519" });
     expect(keyPair.keyID).to.not.empty;
     expect(keyPair.publicKey).to.not.empty;
   });
 
-  it("wallet user closes wallet", async function () {
-    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
-
-    expect((await wallet.close()).closed).to.be.true;
-    expect((await wallet.close()).closed).to.be.false;
-
-    // any operation should fail
-    expect(wallet.getAll({ auth, contentType: contentTypes.METADATA })).to
-      .eventually.be.rejected;
-  });
-});
-
-describe.only("Lite wallet tests", async function () {
-  it("wallet user creates wallet profile", async function () {
-    await createWalletProfile(walletAgent, WALLET_USER, {
-      localKMSPassphrase: testConfig.walletUserPassphrase,
-    });
-  });
-
-  let auth;
-  let wallet;
-
-  it("wallet user opens wallet", async function () {
-    wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
-    let authResponse = await wallet.open({
-      localKMSPassphrase: testConfig.walletUserPassphrase,
-    });
-    expect(authResponse.token).to.not.empty;
-    auth = authResponse.token;
-  });
-
-
-  let publicDID
+  let publicDID;
   it("wallet user creates an Orb DID", async function () {
-    let didManager = new DIDManager({agent: walletAgent, user: WALLET_USER})
+    let didManager = new DIDManager({ agent: walletAgent, user: WALLET_USER });
     let didResolution = await didManager.createOrbDID(auth, {
       keyType: "ED25519",
-      purposes: ["assertionMethod", "authentication"]
-    })
-    expect(didResolution).to.not.empty
+      purposes: ["assertionMethod", "authentication"],
+    });
+    expect(didResolution).to.not.empty;
 
-    publicDID = didResolution.didDocument
+    publicDID = didResolution.didDocument;
   });
 
-  let cred
+  let cred;
   it("wallet user issues self-issued credential", async function () {
     let templateCred = {
       "@context": [
         "https://www.w3.org/2018/credentials/v1",
-        "https://www.w3.org/2018/credentials/examples/v1"
+        "https://www.w3.org/2018/credentials/examples/v1",
       ],
-      "credentialSchema": [{
-        "id": "https://www.w3.org/2018/credentials/v1",
-        "type": "JsonSchemaValidator2018"
-      }],
-      "credentialSubject": {
-        "degree": {
-          "type": "BachelorDegree",
-          "university": "MIT"
+      credentialSchema: [
+        {
+          id: "https://www.w3.org/2018/credentials/v1",
+          type: "JsonSchemaValidator2018",
         },
-        "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-        "name": "Jayden Doe",
-        "spouse": "did:example:c276e12ec21ebfeb1f712ebc6f1"
+      ],
+      credentialSubject: {
+        degree: {
+          type: "BachelorDegree",
+          university: "MIT",
+        },
+        id: "did:example:ebfeb1f712ebc6f1c276e12ec21",
+        name: "Jayden Doe",
+        spouse: "did:example:c276e12ec21ebfeb1f712ebc6f1",
       },
-      "expirationDate": "2020-01-01T19:23:24Z",
-      "id": "http://example.edu/credentials/1872",
-      "issuanceDate": "2010-01-01T19:23:24Z",
-      "issuer": {
-        "id": publicDID.id,
-        "name": "Example University"
+      expirationDate: "2020-01-01T19:23:24Z",
+      id: "http://example.edu/credentials/1872",
+      issuanceDate: "2010-01-01T19:23:24Z",
+      issuer: {
+        id: publicDID.id,
+        name: "Example University",
       },
-      "referenceNumber": 83294847,
-      "type": [
-        "VerifiableCredential",
-        "UniversityDegreeCredential"
-      ]
+      referenceNumber: 83294847,
+      type: ["VerifiableCredential", "UniversityDegreeCredential"],
     };
 
     cred = await wallet.issue(auth, templateCred, {
@@ -227,8 +183,8 @@ describe.only("Lite wallet tests", async function () {
   });
 
   it("wallet user verifies credential", async function () {
-    let res = await wallet.verify(auth, {rawCredential: cred.credential});
-    expect(res) .to.not.empty;
+    let res = await wallet.verify(auth, { rawCredential: cred.credential });
+    expect(res).to.not.empty;
     expect(res.error).to.undefined;
     expect(res.verified).to.eq(true);
   });
@@ -236,90 +192,105 @@ describe.only("Lite wallet tests", async function () {
   // TODO: query by pEx
 
   it("wallet user stores credential", async function () {
-    let res = await wallet.add({auth, contentType: contentTypes.CREDENTIAL, content: cred.credential});
+    let res = await wallet.add({
+      auth,
+      contentType: contentTypes.CREDENTIAL,
+      content: cred.credential,
+    });
     expect(res).to.empty;
   });
 
-  it( "wallet user queries by example", async function () {
+  it("wallet user queries by example", async function () {
     let queryByEx = {
-      "reason": "Please present your identity document.",
-      "example": {
+      reason: "Please present your identity document.",
+      example: {
         "@context": [
           "https://www.w3.org/2018/credentials/v1",
-          "https://www.w3.org/2018/credentials/examples/v1"
+          "https://www.w3.org/2018/credentials/examples/v1",
         ],
-        "type": ["UniversityDegreeCredential"],
-        "trustedIssuer": [
+        type: ["UniversityDegreeCredential"],
+        trustedIssuer: [
           {
-            "required": true,
-            "issuer": publicDID.id
-          }
+            required: true,
+            issuer: publicDID.id,
+          },
         ],
-        "credentialSubject": {
-          "id": "did:example:ebfeb1f712ebc6f1c276e12ec21"
+        credentialSubject: {
+          id: "did:example:ebfeb1f712ebc6f1c276e12ec21",
         },
-        "credentialSchema": {
-          "id": "https://www.w3.org/2018/credentials/v1",
-          "type": "JsonSchemaValidator2018"
-        }
-      }
-    }
+        credentialSchema: {
+          id: "https://www.w3.org/2018/credentials/v1",
+          type: "JsonSchemaValidator2018",
+        },
+      },
+    };
 
-    let res = await wallet.query(auth, [{ "type": "QueryByExample", "credentialQuery": [queryByEx]}]);
+    let res = await wallet.query(auth, [
+      { type: "QueryByExample", credentialQuery: [queryByEx] },
+    ]);
     expect(res).to.not.empty;
     expect(res.results).to.not.undefined;
     expect(res.results).to.not.empty;
   });
 
-  it( "wallet user queries by presentation definition", async function () {
+  it("wallet user queries by presentation definition", async function () {
     let presDef = {
-      "id": "ec2f83c5-eac4-4d04-b41e-6636d6670a2e",
-      "input_descriptors": [{
-        "id": "105b1d58-71f8-4d1e-be71-36c9c6f600c9",
-        "constraints": {
-          "fields": [
-            {
-              "path": [
-                "$.issuer.id"
-              ],
-              "filter": {
-                "type": "string",
-                "const": publicDID.id
-              }
-            }
-          ]
+      id: "ec2f83c5-eac4-4d04-b41e-6636d6670a2e",
+      input_descriptors: [
+        {
+          id: "105b1d58-71f8-4d1e-be71-36c9c6f600c9",
+          constraints: {
+            fields: [
+              {
+                path: ["$.issuer.id"],
+                filter: {
+                  type: "string",
+                  const: publicDID.id,
+                },
+              },
+            ],
+          },
+          format: { jwk: { alg: "EdDSA" } },
         },
-        "format":{"jwk":{"alg": "EdDSA"}}
-      }]
-    }
+      ],
+    };
 
-    let res = await wallet.query(auth, [{ "type": "PresentationExchange", "credentialQuery": [presDef]}]);
+    let res = await wallet.query(auth, [
+      { type: "PresentationExchange", credentialQuery: [presDef] },
+    ]);
     expect(res).to.not.empty;
     expect(res.results).to.not.undefined;
     expect(res.results).to.not.empty;
   });
 
-  let presentation
+  let presentation;
   it("wallet user creates presentation", async function () {
-    presentation = await wallet.prove(auth, {
-        rawCredentials: [cred.credential]
+    presentation = await wallet.prove(
+      auth,
+      {
+        rawCredentials: [cred.credential],
       },
       {
         controller: publicDID.id,
-        proofFormat: "ExternalJWTProofFormat"
-      });
-    expect(presentation) .to.not.empty;
+        proofFormat: "ExternalJWTProofFormat",
+      }
+    );
+    expect(presentation).to.not.empty;
     expect(presentation.presentation).to.not.undefined;
   });
 
   it("wallet user verifies presentation", async function () {
-    let res = await wallet.verify(auth, {presentation: presentation.presentation});
-    expect(res) .to.not.empty;
+    let res = await wallet.verify(auth, {
+      presentation: presentation.presentation,
+    });
+    expect(res).to.not.empty;
     expect(res.error).to.undefined;
     expect(res.verified).to.eq(true);
   });
 
   it("wallet user closes wallet", async function () {
+    let wallet = new UniversalWallet({ agent: walletAgent, user: WALLET_USER });
+
     expect((await wallet.close()).closed).to.be.true;
     expect((await wallet.close()).closed).to.be.false;
 
